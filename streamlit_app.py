5c12379a87d7139477ab7359cf41388e3206e67f
Jefferson Mendoza <91968164+jefferson-mendoza@users.noreply.github.com>
2025-08-11 02:45:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,9 +73,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_list += fruit_chosen + ' '
       
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON']
-        search_on=search_on.reset_index()
-        search_on=search_on.iloc[0]
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[-1]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         st.subheader(fruit_chosen + ' Nutrition Information')
